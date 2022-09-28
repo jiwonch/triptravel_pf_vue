3cad3704c01ddb234b22880436e71265f5399db6
jiwonch <rizjw26@gmail.com>
2022-09-28 18:01:52 +0900

    20220928_vue 프로젝트 시작

---
+++ js/about.774bdb87.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktravel"]=self["webpackChunktravel"]||[]).push([[443],{9095:function(n,r,t){t.r(r),t.d(r,{default:function(){return v}});var u=t(3396);const e={class:"about"},a=(0,u._)("div",{id:"logosub"},null,-1),s=[a];function c(n,r){return(0,u.wg)(),(0,u.iD)("div",e,s)}var l=t(89);const i={},o=(0,l.Z)(i,[["render",c]]);var v=o}}]);
+//# sourceMappingURL=about.774bdb87.js.map
